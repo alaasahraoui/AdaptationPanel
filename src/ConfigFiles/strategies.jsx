75acbab9f6f912ab89a0db5718abef8cbe7eeb9e
alaasahraoui <sahraoui.alaaeddine@gmail.com>
2024-02-17 22:21:31 +0100

    added json files (all good )

---
+++ src/ConfigFiles/strategies.jsx
@@ -0,0 +1,20 @@
+const strategies=
+[
+    {
+      "strategy_id": 1,
+      "name": "Reduce Cognitive Load Strategy",
+  
+    },
+    {
+      "strategy_id": 2,
+      "name": "Redistribution Strategy",
+
+    },
+    {
+      "strategy_id": 3,
+      "name": "Replacement Strategy",
+
+    }
+  ];
+  export default strategies;
+  
