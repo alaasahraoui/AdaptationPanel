bda1309959a9e64c258a0906d40390fe86c1f883
alaasahraoui <sahraoui.alaaeddine@gmail.com>
2024-02-18 01:10:28 +0100

    render 2nd menu only if 1st is selected + fixed the warning + optimized the code + bugs fixed (all working)

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,9 +1,5 @@
-// FormComponent.js
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
-import { action1 } from '../ConfigFiles/action1';
-import { action2 } from '../ConfigFiles/action2';
-import { action3 } from '../ConfigFiles/action3';
 import actions from '../ConfigFiles/actions';
 import strategies from '../ConfigFiles/strategies';
 
@@ -11,12 +7,19 @@ const FormComponent = () => {
   const [selectedStrategy, setSelectedStrategy] = useState('');
   const [selectedAction, setSelectedAction] = useState('');
 
-  // Filter actions based on the selected strategy ( gets updated when the user selects a strategy (reexecutes the function))
-  const filteredActions = actions.filter(action => 
-    action.strategyId === parseInt(selectedStrategy)
-  );
-  //debugging
-console.log(filteredActions);
+  // useMemo to filter actions based on the selected strategy, optimizing re-renders
+  const filteredActions = useMemo(() => actions.filter(action => 
+    action.strategyId === parseInt(selectedStrategy, 10)), [actions, selectedStrategy]);
+
+  const handleStrategyChange = (e) => {
+    setSelectedStrategy(e.target.value);
+    setSelectedAction(''); // Reset selected action when strategy changes
+  };
+
+  const handleActionChange = (e) => {
+    setSelectedAction(e.target.value);
+  };
+
   return (
     <div>
       <FormControl fullWidth>
@@ -25,32 +28,32 @@ console.log(filteredActions);
           labelId="strategy-select-label"
           id="strategy-select"
           value={selectedStrategy}
-          onChange={(e) => setSelectedStrategy(e.target.value)}
+          onChange={handleStrategyChange}
           displayEmpty
         >
-           
+          
           {strategies.map((strategy) => (
-            <MenuItem key={strategy.strategy_id} value={strategy.strategy_id}>
+            <MenuItem key={strategy.strategy_id} value={strategy.strategy_id.toString()}>
               {strategy.name}
             </MenuItem>
           ))}
         </Select>
       </FormControl>
 
-      {selectedStrategy != null && (
-        //i added the != null to remove the bug if the id == 0 it will not show the action of that strategy
+      {/* Render action dropdown only if a strategy is selected */}
+      {selectedStrategy && (
         <FormControl fullWidth style={{ marginTop: '20px' }}>
           <InputLabel id="action-select-label">Action</InputLabel>
           <Select
             labelId="action-select-label"
             id="action-select"
             value={selectedAction}
-            onChange={(e) => setSelectedAction(e.target.value)}
+            onChange={handleActionChange}
             displayEmpty
           >
-           
+            
             {filteredActions.map((action) => (
-              <MenuItem key={action.actionId} value={action.actionId}>
+              <MenuItem key={action.actionId} value={action.actionId.toString()}>
                 {action.actionName}
               </MenuItem>
             ))}
